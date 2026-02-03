e98a2560c286affcafa68c6f9842cd26fff7075d
SachinBandara <sachin@fireflyapps.co.uk>
2026-02-03 23:14:15 +0530

    Buildspec added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  output: 'export', // Next.js 13+
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
